c1e451ec7bad1f2b51ca9c6ae5fe8ed8840241a2
Mitch Alforque <malforque717@gmail.com>
2018-06-29 22:44:05 +0800

    Added landing page components and modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AppMaterialModule } from './app.material.modules';
+import { CarouselComponent } from './shared/carousel/carousel.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
