ce0784b0b2946a18a36c1a0c94ba6e1bb4ed6799
Mitch Alforque <malforque717@gmail.com>
2018-07-19 01:52:58 +0800

    Switch to simple slide show

--- frontend/src/app/shared/carousel/carousel.component.ts
+++ frontend/src/app/shared/carousel/carousel.component.ts
@@ -5,10 +5,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  imageUrls = [
+    {
+      url: 'http://via.placeholder.com/350x150', 
+      caption: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+    },
+    {
+      url: 'http://via.placeholder.com/350x200', 
+      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      url: 'http://via.placeholder.com/350x250', 
+      caption: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
-   
+
   }
 
 }
