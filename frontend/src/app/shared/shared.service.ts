665df0d552d385c080532d4e11219ec7bbf9561f
Mitch Alforque <malforque717@gmail.com>
2018-07-20 01:49:01 +0800

    Reusable snackbar service

--- frontend/src/app/shared/shared.service.ts
+++ frontend/src/app/shared/shared.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +8,14 @@ export class SharedService {
 
   constructor(public snackBar: MatSnackBar) { }
 
-  openSnackBar(message: string) {
-    this.snackBar.open(message, '', {
-      duration: 3000,
+  openSnackBar(message?: string, action?: string, options?: MatSnackBarConfig) {
+    const _message = message ? message : 'Retrieving data from server. Please wait..';
+    const _action = action;
+    const _options: MatSnackBarConfig = {
       verticalPosition: 'top',
-      horizontalPosition: 'end'
-    });
+      horizontalPosition: 'end',
+      ...options
+    }
+    this.snackBar.open(_message, _action, _options);
   }
 }
