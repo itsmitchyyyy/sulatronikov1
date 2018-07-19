6d566b440fab40c49b38000b15bd8910620a7d0d
Mitch Alforque <malforque717@gmail.com>
2018-07-20 01:47:47 +0800

    Added book component layout

--- frontend/src/app/pages/books/books.component.ts
+++ frontend/src/app/pages/books/books.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
 })
 export class BooksComponent implements OnInit {
+  fakeArray = new Array(12);
 
   constructor() { }
 
