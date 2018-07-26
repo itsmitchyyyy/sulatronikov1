7dc18801d51b807de3471e800df685fc97d35268
Mitch Alforque <malforque717@gmail.com>
2018-07-27 03:16:00 +0800

    Added author list component

---
+++ frontend/src/app/pages/users/authors/components/author-list/author-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-author-list',
+  templateUrl: './author-list.component.html',
+  styleUrls: ['./author-list.component.scss']
+})
+export class AuthorListComponent implements OnInit {
+  fakeArray = new Array(12);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
