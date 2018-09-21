1072694ffdbd10d6a799b489a46199c8151c9e4d
mitch alforque <malforque717@gmail.com>
2018-09-22 07:40:51 +0800

    added manuscript add layout

---
+++ frontend/src/app/pages/users/publishers/components/publishertransaction/manuscript.model.ts
@@ -0,0 +1,9 @@
+interface Manuscript {
+    id?: number,
+    photo?: File,
+    title?: string,
+    type?: string,
+    sypnosis?: string,
+    chapter?: string,
+    authorId?: number
+}
--- frontend/src/app/pages/users/publishers/components/publishertransaction/publishertransaction.component.ts
+++ frontend/src/app/pages/users/publishers/components/publishertransaction/publishertransaction.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-publishertransaction',
@@ -10,14 +13,50 @@ import { ActivatedRoute } from '@angular/router';
 export class PublishertransactionComponent implements OnInit, OnDestroy {
   id: number;
   private subscription = new Map<String, Subscription>();
+  loading: boolean = false;
+  form: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
     this.subscription.set('routeSubscription', this.route.
       params.subscribe(params => {
         this.id = +params['id'];
       }));
+    this.form = this.fb.group({
+      'photo': null,
+      'title': null,
+      'sypnosis': null,
+      'authorID': null,
+      'genreID': null
+    });
+  }
+
+  fileUpload(event) {
+    if (event.target.files.length > 0) {
+      let file = event.target.files[0];
+      this.form.get('photo').setValue(file);
+    }
+  }
+
+  submitBook() {
+    const bookData = this.prepareSave();
+   
+  }
+
+  private prepareSave() {
+    let input = new FormData();
+    input.append('photo', this.form.get('photo').value);
+    input.append('title', this.form.get('title').value);
+    input.append('sypnosis', this.form.get('sypnosis').value);
+    input.append('authorID', this.form.get('authorID').value);
+    input.append('genreID', this.form.get('genreID').value);
+    return input;
   }
 
   ngOnDestroy() {
