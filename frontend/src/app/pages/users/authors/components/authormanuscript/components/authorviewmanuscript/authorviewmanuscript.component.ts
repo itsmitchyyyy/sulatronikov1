4e6dd3149513375063ad172337648ffdcf723e1a
mitch alforque <malforque717@gmail.com>
2018-09-22 05:45:48 +0800

    updated view manuscript

--- frontend/src/app/pages/users/authors/components/authormanuscript/components/authorviewmanuscript/authorviewmanuscript.component.ts
+++ frontend/src/app/pages/users/authors/components/authormanuscript/components/authorviewmanuscript/authorviewmanuscript.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-authorviewmanuscript',
   templateUrl: './authorviewmanuscript.component.html',
   styleUrls: ['./authorviewmanuscript.component.scss']
 })
-export class AuthorviewmanuscriptComponent implements OnInit {
+export class AuthorviewmanuscriptComponent implements OnInit, OnDestroy {
+  id: number;
+  private subscription = new Map<String, Subscription>();
 
-  constructor() { }
+  constructor(
+    private router: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.subscription.set('routerSubscription', this.router.params.subscribe(params => {
+      this.id = +params['id'];
+    }));
   }
 
+  ngOnDestroy() {
+    this.subscription.forEach(sub => sub.unsubscribe());
+  }
 }
