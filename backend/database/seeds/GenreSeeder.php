0ed427e610d65edfc58acf0a148bb78677ee94d2
mitch alforque <malforque717@gmail.com>
2018-09-22 06:24:33 +0800

    added genres

---
+++ backend/database/seeds/GenreSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Genre;
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $genres = [
+            'Action',
+            'Adventure',
+            'Anime',
+            'Classics',
+            'General Fiction',
+            'Historical',
+            'Horror',
+            'Classics',
+            'Non Fiction'
+        ];
+        
+        foreach($genres as $genre){
+            $genra = new Genre();
+            $genra->genreName = $genre;
+            $genra->save();
+        }
+    }
+}
